d3dc4159c84a951fce4c82551cdb203603277a90
Grant Timmerman <granttimmerman@gmail.com>
2015-11-14 18:19:34 -0500

    Fix mongo uri

--- config/secrets.js
+++ config/secrets.js
@@ -21,7 +21,7 @@
 
 module.exports = {
 
-  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://123:1234@ds054308.mongolab.com:54308/harbor',
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test' || 'mongodb://123:1234@ds054308.mongolab.com:54308/harbor',
 
   sessionSecret: process.env.SESSION_SECRET || 'kughhgjh',
 
@@ -29,7 +29,7 @@ module.exports = {
     user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
     password: process.env.MAILGUN_PASSWORD || '29eldds1uri6'
   },
-  
+
   mandrill: {
     user: process.env.MANDRILL_USER || 'hackathonstarterdemo',
     password: process.env.MANDRILL_PASSWORD || 'E1K950_ydLR4mHw12a0ldA'
