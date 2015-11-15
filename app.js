2146a2490a01b4a1063e6129a2c958448af74c56
David Gisser <dmang95@yahoo.com>
2015-11-14 20:18:11 -0500

    fixed twilio issues

--- app.js
+++ app.js
@@ -44,12 +44,13 @@ var app = express();
 /**
  * Twilio webhook routes
  */
+var voice = require('./routes/voice');
+var message = require('./routes/message');
 app.post('/voice', voice.interview);
 app.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
 app.post('/message', message);
 
 //aggregate response data
-app.get('/results', results);
 
 /**
  * Connect to MongoDB.
