9ae86fc5b497a37891ca65e24d380ecf76bfe7be
Jingqi Yang <jingqiy@andrew.cmu.edu>
2015-11-14 20:09:21 -0500

    comment all twillios, now node app.js works

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ var passportConf = require('./config/passport');
 /**
  * Twilio webhook routes
  */
-app.post('/voice', voice.interview);
-app.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
-app.post('/message', message);
+// app.post('/voice', voice.interview);
+// app.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
+// app.post('/message', message);
 
 //aggregate response data
-app.get('/results', results);
+// app.get('/results', results);
 
 /**
  * Create Express server.
@@ -55,12 +55,12 @@ var app = express();
 /**
  * Twilio webhook routes
  */
-app.post('/voice', voice.interview);
-app.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
-app.post('/message', message);
+// app.post('/voice', voice.interview);
+// app.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
+// app.post('/message', message);
 
 //aggregate response data
-app.get('/results', results);
+// app.get('/results', results);
 
 /**
  * Connect to MongoDB.
